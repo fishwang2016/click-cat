d397271e8247d917780166dd1fea5ee01da10629
fishwang2016 <zhenyu2004@hotmail.com>
2016-05-31 17:59:15 +0800

    not able to update the clicks per photo

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(function(){
 
    var model ={
 
+   	        currentCat: null,
+
 		   	init: function(){
 		   	    console.log("begin initializing model ...")
 
@@ -35,7 +37,12 @@ $(function(){
 		    getAllCats: function(){
 		    	return JSON.parse(localStorage.cats);
 
-		          }
+		          },
+
+		    updateClicks: function(obj){
+
+
+		    }
          
         };
 
@@ -51,16 +58,26 @@ $(function(){
 		   		return model.getAllCats() ;
 		        	} ,
 
-		    getCurrentCat: funcion(catcopy){ 
-		    	   cat: catcopy.cat,
-		    	   url: catcopy.url,
-		    	   clicks:catcopy.clicks
+		    getCurrentCat: function(){ 
+
+		    	return model.currentCat
 
 		              },
 
+		    setCurrentCat: function(cat){
+		    	model.currentCat = cat;
+
+		    },
+		    incrementCounter: function(){
+		    	model.currentCat.clicks++;
+		    	view.render();
+
+		    },
+
 		   	init: function(){
 
-		   		console.log("initializing octopus ...")
+		   		console.log("initializing octopus ...");
+		   		model.currentCat = model.getAllCats[0];
 
 		   		model.init();
 		   		view.init();
@@ -82,35 +99,29 @@ $(function(){
 		   	render:function(){
 
 		   		var htmlStr =" ";
-		   		var catcontainer;
-		   		var catCaption ;
-
-
+		   		var catcontainer  ;
+				var catCaption ;
+		   		var catImg = document.createElement("img")
 		   		octopus.getCats().forEach(function(cat){
 
-		   			   catCaption = cat.cat +'clicks:' + cat.clicks;
+		   			    catCaption = cat.cat +'clicks:' + cat.clicks;
+		   			    catImg.src = cat.url;
 
-		   			    catcontainer = '<div class="catcontainer"> '+ catCaption+'<img  src="' +cat.url+ '"' + 'alt="'+cat.cat+'">'+  ' </div>';
+		   			    catcontainer = '<div class="catcontainer">'+ catCaption+'<img  '+' id="'+cat.cat+'"'+'src="' +cat.url+ '"' + 'alt="'+cat.cat+'">'+  ' </div>';
+		   			    //catcontainer = '<div class="catcontainer"> '+ catCaption+ catImg + 'alt="'+cat.cat+'">'+  ' </div>';
 
 		   			    htmlStr=htmlStr+ catcontainer;
-
-		   			    $('.catcontainer').addEventListner('click',(function(catcopy){
-
-		   			    	return function(){
-
-		   			    		catcopy.clicks +=1;
-
-		   			    	}
-
-		   			    })(cat));
-
+		   			    console.log("Cat Name: "+cat.cat);
+		   			    var el = document.getElementById(String(cat.cat));
+		   			    console.log(el);
+		   			    // el.addEventListener('click',function(){
+		   			    // 	console.log("hi");
+		   			    // });
 		   		});
 
 		   		this.catList.html(htmlStr);
 		   		//console.log(htmlStr);
-
 		   	}
-
 
    };
    console.log("initialze octopus...");
