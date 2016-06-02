5830d681176b9a7f3c41c50b9344990b059e3d9b
fishwang2016 <zhenyu2004@hotmail.com>
2016-06-02 15:10:22 +0800

    added clicks function and seperate Cat from ViewModel

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-var ViewModel = function(){
+var Cat = function(){
+
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('cat1.jpg');
     this.imgAttribution = ko.observable('attribution');
 
-    this.incrementCounter = function(){
-    	this.clickCount(this.clickCount()+1);
-    };
+    
+
+    this.title = ko.computed(function(){
+    	var title;
+    	var clicks = this.clickCount();
+
+	    if(clicks<10){
+	    		title = 'Newbone';
+	    	} else if (clicks <50){
+
+	    		title ='Infant';
+
+	    	}else if (clicks<100){
+
+	    		title ='Child';
+
+	    	} else{
+
+	    		title ='Adult';
+	    	}	
+	    	return title;
+    },this);// without passing in this ,throw err .
+}
+
+
+
+var ViewModel = function(){
+	this.currentCat = ko.observable(new Cat());
+	this.incrementCounter = function(){
+    	this.currentCat().clickCount(this.currentCat().clickCount()+1);
+    };    
 
 }
 
